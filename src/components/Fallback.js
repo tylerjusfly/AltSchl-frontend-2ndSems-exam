ce32c5c1798f82caf1f810ab1802710fc4201f3c
tylerjusfly <tylerjusfly1@gmail.com>
2022-11-06 14:05:41 +0100

    fixing mobile dashboard responsiveness

--- src/components/Fallback.js
+++ src/components/Fallback.js
@@ -6,12 +6,12 @@ export const FallBackError = ({ error }) => {
     <>
       <Navbar />
       <main className="flex-middle">
-        <h2 className="text-2xl font-ty font-bold">Something went wrong:</h2>
+        <h2 className="text-xl font-ty font-bold">Something went wrong:</h2>
         <img src={warnSign} alt="warning signLogo" width={340} />
-        <pre className="font-bold font-ty text-2xl mt-5" style={{ color: "red" }}>
+        <pre className="font-bold font-ty text-l mt-5" style={{ color: "red" }}>
           {error.message}
         </pre>
-        <p className="mt-5 text-xl font-ty" style={{ color: "red" }}>
+        <p className="mt-5 text-l font-ty" style={{ color: "red" }}>
           Click on web Icon and refresh to go back to home page.
         </p>
       </main>
