64c0c0939f7d988e9624323e74b5906de7b0e4c7
tylerjusfly <tylerjusfly1@gmail.com>
2022-11-06 11:42:08 +0100

    responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,9 +16,9 @@ export const Home = () => {
     // you can add React.Fragment if you wish to add styles to this tag
     <>
       <main className="flex flex-col items-start h-96 m-24">
-        <p className="font-fira word-space">
+        <div className="font-fira word-space">
           SO, YOU WANT TO TRAVEL TO <p className="pt-5 text-8xl font-ty word-space"> SPACE </p>
-        </p>
+        </div>
         <p className="w-80 mt-7 font-ty leading-7">
           Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of
           on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,23 +6,31 @@ export const Navbar = () => {
   const { user, logOut } = useAuthContext();
 
   return (
-    <nav className="flex flex-row justify-between p-4">
-      <Link to="/">
+    <nav className="flex flex-row justify-between p-5 ">
+      <Link to="/" className="self-center">
         <img src={navLogo} alt="nav Logo" width={44} />
       </Link>
-      <ul className="flex flex-row gap-12 font-bold font-fira pr-5 self-center">
-        <Link to={user ? "users" : "login"}>Dashboard {user && `| ${user}`}</Link>
-        <Link to="errorboundary">ErrorBoundary</Link>
-        {user ? (
-          <>
-            <Link onClick={logOut} to="/">
-              LogOut
-            </Link>
-          </>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
-      </ul>
+
+      <div className="parentDiv">
+        <div className="p-4 space-y-2 rounded shadow self-center lg:hidden md:hidden drop--btn">
+          <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
+          <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
+          <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
+        </div>
+        <ul className="flex flex-col gap-12 md:flex-row lg:flex-row font-bold font-fira pr-5 self-center custom-hidden dropdown--content">
+          <Link to={user ? "users" : "login"}>Dashboard {user && `| ${user}`}</Link>
+          <Link to="errorboundary">ErrorBoundary</Link>
+          {user ? (
+            <>
+              <Link onClick={logOut} to="/">
+                LogOut
+              </Link>
+            </>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 };
